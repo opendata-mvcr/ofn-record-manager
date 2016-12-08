3f2b271827c3db9404c4538532acc0213cf68b6b
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2016-12-08 17:03:15 +0100

    [Fix] Show typeahead option description in title.

--- src/main/webapp/js/components/typeahead/TypeaheadResultList.js
+++ src/main/webapp/js/components/typeahead/TypeaheadResultList.js
@@ -1,25 +1,24 @@
 'use strict';
 
-var React = require('react');
+const React = require('react');
 
-var TypeaheadResultList = React.createClass({
+const TypeaheadResultList = React.createClass({
 
     render: function () {
-        var listCls = this.props.options.length < 21 ? 'autocomplete-results' : 'autocomplete-results extended';
+        let listCls = this.props.options.length < 21 ? 'autocomplete-results' : 'autocomplete-results extended';
         if (this.props.customClasses.results) {
             listCls += ' ' + this.props.customClasses.results;
         }
-        var items = [];
-        for (var i = 0, len = this.props.options.length; i < len; i++) {
-            var onClick = this.onClick.bind(this, this.props.options[i]);
-            items.push(<li className='btn-link item' key={'typeahead-result-' + i}
-                           onClick={onClick}>{this.getOptionLabel(this.props.options[i])}</li>);
+        const items = [];
+        for (let i = 0, len = this.props.options.length; i < len; i++) {
+            let option = this.props.options[i],
+                onClick = this.onClick.bind(this, option);
+            items.push(<li className='btn-link item' key={'typeahead-result-' + i} title={option.description}
+                           onClick={onClick}>{this.getOptionLabel(option)}</li>);
         }
-        return (
-            <ul className={listCls}>
-                {items}
-            </ul>
-        );
+        return <ul className={listCls}>
+            {items}
+        </ul>;
     },
 
     getOptionLabel: function (option) {
@@ -30,7 +29,7 @@ var TypeaheadResultList = React.createClass({
         }
     },
 
-    onClick: function(option, event) {
+    onClick: function (option, event) {
         event.preventDefault();
         this.props.onOptionSelected(option, event);
     }
