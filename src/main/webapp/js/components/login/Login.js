34e4486164c0ba7f4e8f7d5b45f5fa7c8b6cd094
Tomáš Klíma <TOMAS.klim@gmail.com>
2018-01-26 09:53:27 +0100

    [upd] Login component ES5 to ES6

--- src/main/webapp/js/components/login/Login.js
+++ src/main/webapp/js/components/login/Login.js
@@ -1,94 +1,91 @@
 'use strict';
 
-var React = require('react');
-var Panel = require('react-bootstrap').Panel;
-var Button = require('react-bootstrap').Button;
-var Alert = require('react-bootstrap').Alert;
-var Form = require('react-bootstrap').Form;
+import React from "react";
+import {Alert, Button, Form, Panel} from "react-bootstrap";
+import HorizontalInput from "../HorizontalInput";
+import Authentication from "../../utils/Authentication";
+import I18nWrapper from "../../i18n/I18nWrapper";
+import injectIntl from "../../utils/injectIntl";
+import Mask from "../Mask";
+import Input from "../Input";
 
-var injectIntl = require('../../utils/injectIntl');
-
-var Mask = require('../Mask').default;
-var Input = require('../HorizontalInput').default;
-var Routing = require('../../utils/Routing');
-var Routes = require('../../utils/Routes');
-var Authentication = require('../../utils/Authentication');
-var I18nMixin = require('../../i18n/I18nMixin');
-
-
-var Login = React.createClass({
-    mixins: [I18nMixin],
-
-    getInitialState: function () {
-        return {
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.i18n = this.props.i18n;
+        this.state = {
             username: '',
             password: '',
             alertVisible: false,
             mask: false
         }
-    },
+    }
 
-    componentDidMount: function () {
-        this.refs.usernameField.focus();
-    },
+    componentDidMount() {
+        this.usernameField.focus();
+    }
 
-    onChange: function (e) {
-        var state = this.state;
+    onChange = (e) => {
+        let state = this.state;
         state[e.target.name] = e.target.value;
         state.alertVisible = false;
         this.setState(state);
-    },
+    };
 
-    onKeyPress: function (e) {
+    onKeyPress = (e) => {
         if (e.key === 'Enter') {
             this.login();
         }
-    },
+    };
 
-    onLoginError: function () {
+    onLoginError = () => {
         this.setState({alertVisible: true, mask: false});
-    },
+    };
 
-    login: function () {
+    login = () => {
         Authentication.login(this.state.username, this.state.password, this.onLoginError);
         this.setState({mask: true});
-    },
+    }
 
-    register: function () {
+    /*
+    register() {
         Routing.transitionTo(Routes.register);
-    },
+    }*/
 
 
-    render: function () {
-        var mask = this.state.mask ? (<Mask text={this.i18n('login.progress-mask')}/>) : null;
-        return <Panel header={<h3>{this.i18n('login.title')}</h3>} bsStyle='info' className="login-panel">
-            {mask}
-            <Form horizontal>
-                {this.renderAlert()}
-                <Input type='text' name='username' ref='usernameField' label={this.i18n('login.username')}
-                       value={this.state.username}
-                       onChange={this.onChange} labelWidth={3} onKeyPress={this.onKeyPress}
-                       inputWidth={9}/>
-                <Input type='password' name='password' label={this.i18n('login.password')}
-                       value={this.state.password}
-                       onChange={this.onChange} labelWidth={3} onKeyPress={this.onKeyPress}
-                       inputWidth={9}/>
-                <Button bsStyle='link login-forgot-password' bsSize='small'
-                        disabled={this.state.mask}>Forgot password</Button>
-                <div className="login-buttons">
-                    <Button bsStyle='success' bsSize='large' class='login-button-login' onClick={this.login}
-                            disabled={this.state.mask}>{this.i18n('login.submit')}</Button>
-                    {/*TODO i18n forgot password, click*/}
-                </div>
-            </Form>
-        </Panel>;
-    },
-
-    renderAlert: function () {
+    renderAlert() {
         return this.state.alertVisible ? <Alert bsStyle='danger' bsSize='small'>
             <div>{this.i18n('login.error')}</div>
         </Alert> : null;
     }
-});
 
-module.exports = injectIntl(Login);
+    render() {
+        const mask = this.state.mask ? (<Mask text={this.i18n('login.progress-mask')}/>) : null;
+        return(<Panel header={<h3>{this.i18n('login.title')}</h3>} bsStyle='info' className="login-panel">
+            {mask}
+            <Form horizontal>
+                {this.renderAlert()}
+                <HorizontalInput type='text' name='username' ref={(input) => { this.usernameField = input; }}
+                       label={this.i18n('login.username')} value={this.state.username}
+                       onChange={this.onChange} labelWidth={3} onKeyPress={this.onKeyPress}
+                       inputWidth={9}/>
+                <HorizontalInput type='password' name='password' label={this.i18n('login.password')}
+                       value={this.state.password}
+                       onChange={this.onChange} labelWidth={3} onKeyPress={this.onKeyPress}
+                       inputWidth={9}/>
+                <div className="login-forgot-password-block">
+                    <Button bsStyle='link' className='login-forgot-password' bsSize='small'
+                            disabled={this.state.mask}>Forgot password</Button>
+                </div>
+                <div className="login-button">
+                    <Button bsStyle='success' bsSize='large' className='login-button-login' onClick={this.login}
+                            disabled={this.state.mask}>{this.i18n('login.submit')}</Button>
+                    {/*TODO i18n forgot password, click*/}
+                </div>
+            </Form>
+        </Panel>
+        )
+    }
+}
+
+export default injectIntl(I18nWrapper(Login));
