366482d24b442561f01326597dd4ab569b0b7d85
Tomáš Klíma <tomas.klim@gmail.com>
2018-02-18 16:56:06 +0100

    [upd] UserRow in separate file, user deletion refactor

---
+++ src/main/webapp/js/components/user/UserRow.js
@@ -0,0 +1,34 @@
+import React from "react";
+import injectIntl from "../../utils/injectIntl";
+import I18nWrapper from "../../i18n/I18nWrapper";
+import Routes from "../../utils/Routes";
+import {Button} from "react-bootstrap";
+
+let UserRow = (props) => {
+    const user = props.user;
+    return <tr>
+        <td className='report-row'>
+            <a href={'#/' + Routes.users.path + '/' + user.username}
+               title={props.i18n('users.open-tooltip')}>{user.firstName + ' ' + user.lastName}</a>
+        </td>
+        <td className='report-row'>{user.username}</td>
+        <td className='report-row'>{user.institution ? user.institution.name : ''}</td>
+        <td className='report-row'>{user.emailAddress}</td>
+        <td className='report-row actions'>
+            <Button bsStyle='primary' bsSize='small' title={props.i18n('users.open-tooltip')}
+                    onClick={() => props.onEdit(props.user)}>{props.i18n('open')}</Button>
+            <Button bsStyle='warning' bsSize='small' title={props.i18n('users.delete-tooltip')}
+                    onClick={() => props.onDelete(props.user)}>{props.i18n('delete')}
+                    {props.deletionLoading && <div className="loader"></div>}</Button>
+        </td>
+    </tr>;
+};
+
+UserRow.propTypes = {
+    user: React.PropTypes.object.isRequired,
+    onEdit: React.PropTypes.func.isRequired,
+    onDelete: React.PropTypes.func.isRequired,
+    deletionLoading: React.PropTypes.bool.isRequired
+};
+
+export default injectIntl(I18nWrapper(UserRow));
--- src/main/webapp/js/components/user/UserTable.js
+++ src/main/webapp/js/components/user/UserTable.js
@@ -6,6 +6,7 @@ import DeleteItemDialog from "../DeleteItemDialog";
 import injectIntl from "../../utils/injectIntl";
 import I18nWrapper from "../../i18n/I18nWrapper";
 import Routes from "../../utils/Routes";
+import UserRow from "./UserRow";
 
 class UserTable extends React.Component {
     static propTypes = {
@@ -73,39 +74,11 @@ class UserTable extends React.Component {
             userDeleted = this.props.userDeleted;
         let rows = [];
         for (let i = 0, len = users.length; i < len; i++) {
-            rows.push(<UserRow key={users[i].username} user={users[i]} onEdit={onEdit} onDelete={this._onDelete} userDeleted={userDeleted}/>);
+            rows.push(<UserRow key={users[i].username} user={users[i]} onEdit={onEdit} onDelete={this._onDelete}
+                               deletionLoading={userDeleted.fetching && userDeleted.username === users[i].username}/>);
         }
         return rows;
     }
 }
-
-let UserRow = (props) => {
-    const user = props.user;
-    return <tr>
-        <td className='report-row'>
-            <a href={'#/' + Routes.users.path + '/' + user.username}
-               title={props.i18n('users.open-tooltip')}>{user.firstName + ' ' + user.lastName}</a>
-        </td>
-        <td className='report-row'>{user.username}</td>
-        <td className='report-row'>{user.institution ? user.institution.name : ''}</td>
-        <td className='report-row'>{user.emailAddress}</td>
-        <td className='report-row actions'>
-            <Button bsStyle='primary' bsSize='small' title={props.i18n('users.open-tooltip')}
-                    onClick={() => props.onEdit(props.user)}>{props.i18n('open')}</Button>
-            <Button bsStyle='warning' bsSize='small' title={props.i18n('users.delete-tooltip')}
-                    onClick={() => props.onDelete(props.user)}>{props.i18n('delete')}{props.userDeleted.fetching && props.userDeleted.username === user.username &&
-                <div className="loader"></div>}</Button>
-        </td>
-    </tr>;
-};
-
-UserRow.propTypes = {
-    user: React.PropTypes.object.isRequired,
-    onEdit: React.PropTypes.func.isRequired,
-    onDelete: React.PropTypes.func.isRequired,
-    userDeleted: React.PropTypes.object
-};
-
-UserRow = injectIntl(I18nWrapper(UserRow));
 
 export default injectIntl(I18nWrapper(UserTable));
