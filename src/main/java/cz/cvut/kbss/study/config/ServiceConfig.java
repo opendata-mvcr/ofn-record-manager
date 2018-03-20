0da7175363d14a1656f88391ec4c40db0f39806b
Tomáš Klíma <tomas.klim@gmail.com>
2018-03-20 10:14:35 +0100

    [upd] password encryption

--- src/main/java/cz/cvut/kbss/study/config/ServiceConfig.java
+++ src/main/java/cz/cvut/kbss/study/config/ServiceConfig.java
@@ -4,10 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @ComponentScan(basePackages = "cz.cvut.kbss.study.service")
 public class ServiceConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public RestTemplate restTemplate() {
