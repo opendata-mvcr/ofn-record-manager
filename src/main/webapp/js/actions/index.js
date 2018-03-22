352c3424430217e261f0cd3c433edb60195e7b80
Tomáš Klíma <tomas.klim@gmail.com>
2018-03-22 13:24:40 +0100

    [upd] Defined response error on 500 status

--- src/main/webapp/js/actions/index.js
+++ src/main/webapp/js/actions/index.js
@@ -15,6 +15,15 @@ axiosBackend.interceptors.response.use(
         if (status === 403) { // non-authorized
             transitionTo(Routes.dashboard);
         }
+        if (status === 500) { // server error
+            error = {
+                response: {
+                    data: {
+                        message: "Something went wrong, please try it again or refresh browser."
+                    }
+                }
+            }
+        }
         return Promise.reject(error);
     }
 );
