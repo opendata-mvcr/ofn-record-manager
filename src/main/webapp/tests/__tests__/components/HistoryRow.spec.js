ceb57e3ff890b7738142d39e533885401af57b07
Tomáš Klíma <tomas.klim@gmail.com>
2018-05-11 23:57:31 +0200

    [upd] Add missing js tests

---
+++ src/main/webapp/tests/__tests__/components/HistoryRow.spec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import React from 'react';
+import {IntlProvider} from 'react-intl';
+import TestUtils from 'react-addons-test-utils';
+import HistoryRow from "../../../js/components/history/HistoryRow";
+
+describe('HistoryRow', function () {
+    const intlData = require('../../../js/i18n/en');
+    let action = {key: "12345", type: "TEST", author: {username: "test"}, timestamp: 1526074842 },
+        onOpen = jasmine.createSpy('onOpen');
+
+    it('renders one row of table with 4 columns and 1 button', function () {
+        const tree = TestUtils.renderIntoDocument(
+            <IntlProvider locale="en" {...intlData}>
+                <table>
+                    <tbody>
+                    <HistoryRow key={action.key} action={action} onOpen={onOpen}/>
+                    </tbody>
+                </table>
+            </IntlProvider>);
+        const td = TestUtils.scryRenderedDOMComponentsWithTag(tree, 'td');
+        expect(td.length).toEqual(4);
+        const buttons = TestUtils.scryRenderedDOMComponentsWithTag(tree, 'Button');
+        expect(buttons.length).toEqual(1);
+    });
+
+    it('renders "Open" button and click on it', function () {
+        const tree = TestUtils.renderIntoDocument(
+            <IntlProvider locale="en" {...intlData}>
+                <table>
+                    <tbody>
+                    <HistoryRow key={action.key} action={action} onOpen={onOpen}/>
+                    </tbody>
+                </table>
+            </IntlProvider>);
+        const buttons = TestUtils.scryRenderedDOMComponentsWithTag(tree, "Button");
+        expect(buttons.length).toEqual(1);
+
+        TestUtils.Simulate.click(buttons[0]); // Open Action
+        expect(onOpen).toHaveBeenCalled();
+    });
+});
---
+++ src/main/webapp/tests/__tests__/components/HistoryTable.spec.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import React from 'react';
+import {IntlProvider} from 'react-intl';
+import TestUtils from 'react-addons-test-utils';
+import HistoryTable from "../../../js/components/history/HistoryTable";
+
+describe('HistoryTable', function () {
+    const intlData = require('../../../js/i18n/en');
+    let actions = [],
+        searchData = {},
+        handlers = {
+            handleSearch: jasmine.createSpy('handleSearch'),
+            handleReset: jasmine.createSpy('handleReset'),
+            handleChange: jasmine.createSpy('handleChange'),
+            onKeyPress: jasmine.createSpy('onKeyPress'),
+            onOpen: jasmine.createSpy('onOpen')
+        };
+
+    it('renders table with 4 headers columns', function () {
+        const tree = TestUtils.renderIntoDocument(
+            <IntlProvider locale="en" {...intlData}>
+                <HistoryTable handlers={handlers} searchData={searchData}
+                              actions={actions}/>
+            </IntlProvider>);
+        const table = TestUtils.scryRenderedDOMComponentsWithTag(tree,'table');
+        expect(table).not.toBeNull();
+        const th = TestUtils.scryRenderedDOMComponentsWithTag(tree,'th');
+        expect(th.length).toEqual(4);
+    });
+});
