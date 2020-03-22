edcbf535b0a2a198a225ddf5fc10f321b6f2bb37
Tomáš Klíma <tomas.klim@gmail.com>
2020-03-22 09:24:05 +0100

    [fix] unable to update user's email

--- src/main/java/cz/cvut/kbss/study/service/repository/RepositoryUserService.java
+++ src/main/java/cz/cvut/kbss/study/service/repository/RepositoryUserService.java
@@ -189,7 +189,7 @@ public class RepositoryUserService extends BaseRepositoryService<User> implement
         if (!userDao.findByUsername(instance.getUsername()).getUri().equals(instance.getUri())) {
             throw new EntityExistsException("User with specified username already exists.");
         }
-        if (!userDao.findByEmail(instance.getEmailAddress()).getUsername().equals(instance.getUsername())) {
+        if (userDao.findByEmail(instance.getEmailAddress()) != null) {
             throw new EntityExistsException("User with specified email already exists.");
         }
         final User orig = userDao.find(instance.getUri());
