c6491485a5dc1c53bbafdc3038d830604d53ab1f
Tomáš Klíma <tomas.klim@gmail.com>
2020-04-25 20:16:16 +0200

    [upd] TypeAhead without reflux

--- src/main/webapp/js/stores/FormGenStore.js
+++ src/main/webapp/js/stores/FormGenStore.js
@@ -1,42 +1,44 @@
 'use strict';
 
-import Reflux from 'reflux';
 import jsonld from 'jsonld';
-import Actions from '../actions/Actions';
 import * as Logger from "../utils/Logger";
 import {axiosBackend} from "../actions";
 import {API_URL} from '../../config';
 
-class FormGenStore extends Reflux.Store {
-    constructor() {
-        super();
-        this.options = {};
-        this.listenTo(Actions.loadFormOptions, this.onLoadFormOptions);
-    }
+const FORM_GEN_POSSIBLE_VALUES_URL = `${API_URL}/rest/formGen/possibleValues`;
 
-    onLoadFormOptions = (id, query) => {
-        if (this.options[id] && this.options[id].length !== 0) {
-            this.trigger(id, this.options[id]);
-            return;
+class FormGenStore {
+    options = {};
+
+    loadFormOptions = async (id, query) => async (resolve, reject) => {
+        const option = this.options[id];
+
+        if (option && option.length) {
+            return resolve(this.options[id]);
         }
 
-        axiosBackend.get(`${API_URL}/rest/formGen/possibleValues?query=${encodeURIComponent(query)}`).then((response) => {
+        try {
+            const response = await axiosBackend.get(`${FORM_GEN_POSSIBLE_VALUES_URL}?query=${encodeURIComponent(query)}`);
             const data = response.data;
-            if (data.length > 0) {
-                jsonld.frame(data, {}, null, (err, framed) => {
-                    this.options[id] = framed['@graph'];
-                    this.trigger(id, this.options[id]);
-                });
-            } else {
-                Logger.warn('No data received when loading options using query' + query + '.');
-                this.trigger(id, this.getOptions(id));
-            }
-        });
-    };
 
-    getOptions = (id) => {
-        return this.options[id] ? this.options[id] : [];
+            if (data.length) {
+                const framed = await jsonld.frame(data, {});
+
+                this.options[id] = framed['@graph'];
+
+                return resolve(this.options[id]);
+            }
+            Logger.warn(`No data received when loading options using query ${query}`);
+
+            return resolve([]);
+        } catch (error) {
+            Logger.error(`An error has occurred during loadFormOptions using ${query}.`);
+
+            return reject(error);
+        }
     }
+
+    getOptions = (id) => this.options[id] || []
 }
 
 export default FormGenStore;
