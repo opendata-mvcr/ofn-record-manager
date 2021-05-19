c902b77ab82ed1106ce8a36e6cda07db7280154f
Miroslav Blasko <blcham@gmail.com>
2021-05-19 15:23:43 +0200

    [Upd] Update docker entrypoint configuration

--- src/main/webapp/.docker/docker-entrypoint.sh
+++ src/main/webapp/.docker/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${API_URL}' < /etc/nginx/config.js.template > /var/www/config.js
+envsubst '${API_URL} ${APP_TITLE} ${LANGUAGE} ${NAVIGATOR_LANGUAGE} ${BASENAME}' < /etc/nginx/config.js.template > /var/www/config.js
 
 exec "$@"
